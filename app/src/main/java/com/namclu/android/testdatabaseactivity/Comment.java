5a8b20ad6fa45d242c787e38235e8394bdd94ac9
Nam <namlu1205@gmail.com>
2016-09-07 17:23:40 +1000

    completed Comment.java, our model and data that will be saved in database and shown in UI

--- app/src/main/java/com/namclu/android/testdatabaseactivity/Comment.java
+++ app/src/main/java/com/namclu/android/testdatabaseactivity/Comment.java
@@ -2,6 +2,33 @@ package com.namclu.android.testdatabaseactivity;
 
 /**
  * Created by namlu on 07-Sep-16.
+ *
+ * Comment.java class is our model and contains the data we will save in the database and show in the user interface
  */
 public class Comment {
+
+    private long id;
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
 }
--- app/src/main/java/com/namclu/android/testdatabaseactivity/MySQLiteHelper.java
+++ app/src/main/java/com/namclu/android/testdatabaseactivity/MySQLiteHelper.java
@@ -21,7 +21,7 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     public static final String DATABASE_CREATE = "create table " +
             TABLE_COMMENTS + "( " +
             COLUMN_ID + " integer primary key autoincrement, " +
-            COLUMN_COMMENT + " textj not null);";
+            COLUMN_COMMENT + " text not null);";
 
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);

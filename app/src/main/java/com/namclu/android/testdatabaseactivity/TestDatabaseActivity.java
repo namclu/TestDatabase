62b5daba285faab9fe9e09628b69c1d33807f91c
Nam <namlu1205@gmail.com>
2016-09-08 12:14:43 +1000

    Completed TestDatabaseActivity.java, app in working state

--- app/src/main/java/com/namclu/android/testdatabaseactivity/TestDatabaseActivity.java
+++ app/src/main/java/com/namclu/android/testdatabaseactivity/TestDatabaseActivity.java
@@ -2,8 +2,11 @@ package com.namclu.android.testdatabaseactivity;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 
 import java.util.List;
+import java.util.Random;
 
 public class TestDatabaseActivity extends ListActivity {
     private CommentsDataSource dataSource;
@@ -16,6 +19,55 @@ public class TestDatabaseActivity extends ListActivity {
         dataSource = new CommentsDataSource(this);
         dataSource.open();
 
-        List 
+        List<Comment> values = dataSource.getAllComments();
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<Comment> adapter = new ArrayAdapter<Comment>(this,
+                android.R.layout.simple_list_item_1,
+                values);
+
+        setListAdapter(adapter);
+
+    }
+
+    // Will be called via the onClick attribute
+    // of the buttons in activity_test_database.xml
+    public void onClick(View view) {
+        @SuppressWarnings("unchecked")
+        ArrayAdapter<Comment> adapter = (ArrayAdapter<Comment>)getListAdapter();
+
+        Comment comment = null;
+        switch (view.getId()) {
+            case R.id.add:
+                String[] comments = new String[] {"Cool", "Very nice", "Hate it"};
+                int nextInt = new Random().nextInt(3);
+
+                // save the new comment to the database
+                comment = dataSource.createComment(comments[nextInt]);
+                adapter.add(comment);
+                break;
+
+            case R.id.delete:
+                if (getListAdapter().getCount() > 0) {
+                    comment = (Comment) getListAdapter().getItem(0);
+                    dataSource.deleteComment(comment);
+                    adapter.remove(comment);
+                }
+                break;
+        }
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        dataSource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        dataSource.close();
+        super.onPause();
     }
 }
